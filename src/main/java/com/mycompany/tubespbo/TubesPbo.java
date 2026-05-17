e12a676e552009d69e5d862aca108b1e0b7ca3ab
Fathir6 <fathirwahidin54@gmail.com>
2026-05-17 20:53:11 +0800

    don

--- src/main/java/com/mycompany/tubespbo/TubesPbo.java
+++ src/main/java/com/mycompany/tubespbo/TubesPbo.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.tubespbo;
 
-/**
- *
- * @author user
- */
 public class TubesPbo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Utama().setVisible(true);
     }
 }
